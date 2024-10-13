1da1eb6ecdc249a07f08bf4c6af07505fe407685
Doraemon <bradley.nguyen@employmenthero.com>
2024-10-13 23:12:33 +0700

    Enhance environment variable validation and cleanup
    Added a function to validate essential environment variables required for operation, ensuring that invalid configurations raise clear errors. Simplified the code structure by consolidating condition checks and improving the readability of reviewer formatting. These changes enhance reliability and maintainability, reducing potential runtime errors and streamlining troubleshooting for users.

--- notify_review.py
+++ notify_review.py
@@ -1,18 +1,21 @@
+import os
+import re
+import requests
+import logging
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 from dotenv import load_dotenv
-import os
-import requests
-import re
-import logging
+
 # Load environment variables from .env file
 load_dotenv()
 
+# Constants
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 USER_ID = os.getenv("USER_ID")
 
+# Initialize Slack client and logger
 client = WebClient(token=BOT_TOKEN)
 logger = logging.getLogger(__name__)
 
@@ -50,72 +53,56 @@ EXTERNAL_ID_MAP = {
     "@engineering-managers": "S05FPFUQKK6",
 }
 
+def validate_env_vars():
+    if not GITHUB_TOKEN:
+        raise ValueError("Invalid GITHUB_TOKEN.")
+    if not BOT_TOKEN:
+        raise ValueError("Invalid BOT_TOKEN.")
+    if not CHANNEL_ID:
+        raise ValueError("Invalid CHANNEL_ID.")
+    if not USER_ID:
+        raise ValueError("Invalid USER_ID.")
+
 def convert_reviewers_to_subteam_format(reviewers):
     subteams = []
     for reviewer in reviewers.split(", "):
         external_id = EXTERNAL_ID_MAP.get(reviewer.strip())
-        if external_id:
-            subteams.append(f"<!subteam^{external_id}>")
-        else:
-            subteams.append(reviewer)
+        subteams.append(f"<!subteam^{external_id}>" if external_id else reviewer)
     return " ".join(subteams)
 
 def send_to_slack(title, reviewers, pr_url, email):
     formatted_reviewers = convert_reviewers_to_subteam_format(reviewers)
-
     message = f"Hi team, please help <@{USER_ID}> review this PR {pr_url} \nSummary: {title} \ncc {formatted_reviewers}"
 
-    if not CHANNEL_ID:
-        raise ValueError("Invalid CHANNEL_ID.")
-    
-    if not BOT_TOKEN:
-        raise ValueError("Invalid BOT_TOKEN.")
-    
-    if not USER_ID:
-        raise ValueError("Invalid USER_ID.")
     try:
-        # Call the chat.postMessage method using the WebClient
-        result = client.chat_postMessage(
-            channel=CHANNEL_ID, 
-            text=message,
-        )
+        result = client.chat_postMessage(channel=CHANNEL_ID, text=message)
         logger.info(result)
-
     except SlackApiError as e:
         logger.error(f"Error posting message: {e}")
 
 def get_user_email(user_login):
     user_url = f"https://api.github.com/users/{user_login}"
     headers = {"Authorization": f"token {GITHUB_TOKEN}"}
-
     user_response = requests.get(user_url, headers=headers).json()
-    email = user_response.get('email', 'No public email')
-
-    return email
+    return user_response.get('email', 'No public email')
 
 def contains_reviewer(reviewers, reviewer_to_check):
     return reviewer_to_check in reviewers
 
 def get_pr_details(pr_url):
-    if not GITHUB_TOKEN:
-        raise ValueError("Invalid GITHUB_TOKEN.")
-    
     match = re.match(r'https://github.com/([^/]+)/([^/]+)/pull/(\d+)', pr_url)
     if not match:
         raise ValueError("Invalid GitHub Pull Request URL format.")
-    organization = match.group(1)
-    repo = match.group(2)
-    pr_number = match.group(3)
+    
+    organization, repo, pr_number = match.groups()
     pr_api_url = f"https://api.github.com/repos/{organization}/{repo}/pulls/{pr_number}"
     headers = {"Authorization": f"token {GITHUB_TOKEN}"}
-
     response = requests.get(pr_api_url, headers=headers).json()
-    state = response.get('state')
-    if state != 'open':
+
+    if response.get('state') != 'open':
         raise ValueError("Pull Request is not open.")
 
     title = response.get('title', 'No title')
-
     reviewers = [f"@{team['name']}" for team in response.get('requested_teams', [])]
     reviewers = ", ".join(reviewers)
 
@@ -130,12 +117,12 @@ def get_pr_details(pr_url):
         reviewers += f", {external_reviewers}"
 
     user_login = response['user']['login']
-
     email = get_user_email(user_login)
 
     send_to_slack(title, reviewers, pr_url, email)
 
 def main():
+    validate_env_vars()
     pr_url = input("Enter the Pull Request URL: ")
 
     if re.match(r"^https://github.com/[^/]+/[^/]+/pull/\d+$", pr_url):
